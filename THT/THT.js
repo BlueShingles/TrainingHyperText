e5dbe42f6cf7bac2ca32e8b9cb34ae4ad8f0ac63
BlueShingles <gubapizxc@hotmail.com>
2024-06-06 18:25:25 -0300

    Melhora de tooltips

--- THT/THT.js
+++ THT/THT.js
@@ -658,6 +658,11 @@ function addToDiv(el, content, placement){
 
 function addTooltip(el){
 	let tooltip = getAt(el,"toolTip");
+	if(tooltip == "") return;
+	let tooltipInner = `<button class="tooltipIcon text-semi-muted m-1 mr-3" data-toggle="tooltip" data-placement="top" title="`+ tooltip +`">🛈</button>` 
+	let tooltipIcon = wrapContent("div", tooltipInner, ["m-0", "float-right", "p-0"], "tooltipOf_"+el.id, [{key:"style", value:"float:right;"}]);
+	let tooltipRow =  wrapContent("div", tooltipIcon, ["w-100"], "tooltipWrapperOf_"+el.id, [{key:"style", value:"height:20px;"}]);
+	addToDiv(el, tooltipRow, "first");
 	if(tooltip == undefined) return;
 	el.setAttribute("title", tooltip);
 }
